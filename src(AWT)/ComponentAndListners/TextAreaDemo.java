a34909755e4b49e5bf82b2e22318c1d828a392b6
sachinsavita <sachinsavita95@gmail.com>
2022-07-15 23:36:13 +0530

    Update

---
+++ src(AWT)/ComponentAndListners/TextAreaDemo.java
@@ -0,0 +1,19 @@
+package ComponentAndListners;
+
+import java.awt.*;
+
+class Frame1 extends Frame
+{
+	TextArea ta;
+	TextField tf;
+	Label l;
+	Button bt;
+}
+public class TextAreaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
